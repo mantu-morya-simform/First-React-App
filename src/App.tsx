8a60cb005f9e2b9ff2450f347f5ae459a0aca90f
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-05-25 14:03:12 +0530

    feat: add UserDetail, ActionButton, Menu, and MenuItem components to enhance app functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import ActionButton from "./components/ActionButton";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
 import CounterIncSame from "./components/CounterIncSame";
+import Menu from "./components/Menu";
 import PrintDetail from "./components/PrintDetail";
 import RenderList from "./components/RenderList";
 import ReusedComponent from "./components/ReusedComponent";
+import UserDetail from "./components/UserDetail";
 
 function App() {
   console.log("hello");
@@ -21,6 +24,14 @@ function App() {
       <CounterIncSame />
       {/*use different counter instance so it separately increment both counter */}
       <Counter />
+      <hr />
+      <UserDetail name="Mantu Morya" isOnline={false} hideOffline={false} />
+      <hr />
+      <ActionButton text="Signup" />
+      <ActionButton text="Send Message" />
+      <ActionButton text="Cart" />
+      <hr />
+      <Menu />
     </>
   );
 }
